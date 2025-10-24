6f947ad7bb4d925620ace0983cb6ccccb11f6417
Đoàn Trần Huỳnh Như <dn8836v@gre.ac.uk>
2025-10-24 17:00:31 +0700

    Add files via upload

---
+++ addjoke.html.php
@@ -0,0 +1,11 @@
+<form action = "" method = "post" enctype="multipart/form-data">
+        <label for="joketext">Type your joke here;</label>
+        <textarea name="joketext"  cols="40" rows="3"></textarea>
+
+        <label for="image"> image:</label>
+       <input type="file" name="image" id="image" accept="image/*">
+
+        <input type="submit" name="submit" value="Add ">
+
+</form>
+       
